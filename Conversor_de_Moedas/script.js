3011b78d037f17336a3c8f8d9bda14b36a9ae0be
AlexandreSoares <soaresallexandre28@gmail.com>
2025-09-15 10:16:01 -0300

    Adicionado a função de converter pra dolar e euro

--- Conversor_de_Moedas/script.js
+++ Conversor_de_Moedas/script.js
@@ -1,13 +1,34 @@
-const convertButton = document.querySelector('.convert-button')
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
 function convertValues() {
-    const inputCurrency = document.querySelector('.input-currency').value;
+  const inputCurrency = document.querySelector(".input-currency").value; //Pegando o valor preenchido no input
+  const currencyToConvert = document.querySelector(
+    ".currency-value-to-convert"
+  );
+  const currencyConverted = document.querySelector(".currency-value");
 
-    const dolarToday = 5.34;
+  const dolarToDay = 5.34;
+  const euroToDay = 6.29;
 
-    const convertedValue = inputCurrency / dolarToday
+  if (currencySelect.value === "dolar") {
+    currencyConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputCurrency / dolarToDay);
+  }
 
-    console.log(convertedValue)
+  if (currencySelect.value === "euro") {
+    currencyConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputCurrency / euroToDay);
+  }
+
+  currencyToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputCurrency);
 }
 
-convertButton.addEventListener('click', convertValues)
+convertButton.addEventListener("click", convertValues);
