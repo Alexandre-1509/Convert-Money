084e12ac9e72bdf38b5af095af1f2ef22fd7c92d
AlexandreSoares <soaresallexandre28@gmail.com>
2025-09-15 09:37:35 -0300

    Alterado a classe do valor a ser convertido, adicionado essa nova classe ao css, e feito mapeamento dos componentes no javascript

---
+++ Conversor_de_Moedas/script.js
@@ -0,0 +1,13 @@
+const convertButton = document.querySelector('.convert-button')
+
+function convertValues() {
+    const inputCurrency = document.querySelector('.input-currency').value;
+
+    const dolarToday = 5.34;
+
+    const convertedValue = inputCurrency / dolarToday
+
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener('click', convertValues)
